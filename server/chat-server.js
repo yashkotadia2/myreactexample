3c51917228f922d7b72253ed94d0240629540e67
yashkotadia2 <97982027+yashkotadia2@users.noreply.github.com>
2023-08-16 13:12:20 +0530

    Update chat-server.js

--- server/chat-server.js
+++ server/chat-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 80;
 const http = require('http').Server(app);
 const cors = require('cors');
 const socketIO = require('socket.io')(http, {
